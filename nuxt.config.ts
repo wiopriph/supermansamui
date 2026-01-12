55390b0f6ce844bd7585f6c2f342caadd7a8e215
Evgeny Korotaev <e.korotaev@robo.finance>
2026-01-12 23:01:36 +0700

    Обновлен конфиг для favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,27 @@ export default defineNuxtConfig({
     enabled: process.env.NODE_ENV !== 'production',
   },
 
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '48x48', href: '/favicon-48x48.png' },
+        { rel: 'icon', type: 'image/png', sizes: '64x64', href: '/favicon-64x64.png' },
+        { rel: 'icon', type: 'image/png', sizes: '128x128', href: '/favicon-128x128.png' },
+        { rel: 'icon', type: 'image/png', sizes: '256x256', href: '/favicon-256x256.png' },
+        { rel: 'icon', type: 'image/png', sizes: '512x512', href: '/favicon-512x512.png' },
+
+        { rel: 'apple-touch-icon', sizes: '57x57', href: '/apple-touch-icon-57x57.png' },
+        { rel: 'apple-touch-icon', sizes: '114x114', href: '/apple-touch-icon-114x114.png' },
+        { rel: 'apple-touch-icon', sizes: '120x120', href: '/apple-touch-icon-120x120.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      ],
+    },
+  },
+
   features: {
     inlineStyles: true,
   },
